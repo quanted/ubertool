aa96175fea9c6f0225cba68668c137fcaad1b734
Tom Purucker <purucker.tom@gmail.com>
2016-01-07 20:37:12 -0500

    csv update

--- ubertool/agdrift/agdrift_create_sqlite.py
+++ ubertool/agdrift/agdrift_create_sqlite.py
@@ -5,7 +5,7 @@ con = sqlite3.connect(":memory:")
 cur = con.cursor()
 cur.execute("CREATE TABLE t (col1, col2);")
 
-with open('agdrift_database.csv','rb') as fin: # `with` statement available in 2.5+
+with open('agdrift_database.csv','rb') as fin:
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
     to_db = [(i['col1'], i['col2']) for i in dr]
