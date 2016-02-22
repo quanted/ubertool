384fb1184c11f07354a640a37793375111b7e687
Tom Purucker <purucker.tom@gmail.com>
2016-02-21 22:27:12 -0500

    setup.py edits

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
-from distutils.core import setup
+from __future__ import print_function
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+#import different_models
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
 
 # create package with specific modules accessible
 # https://packaging.python.org/en/latest/distributing/
 # run python setup.py sdist
 
 setup(name='ubertool',
-      version='1.0',
+      version=ubertool.__version__,
       description='ubertool ecological risk models',
       author='Tom Purucker',
       author_email='purucker.tom@epa.gov',
