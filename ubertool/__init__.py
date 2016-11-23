420ff6b5dc13889c4e767f6e1019e9b568d8986d
Gerry Laniak <laniak.gerry@epa.gov>
2016-11-23 12:55:59 -0500

    minor changes to assertion reporting

--- ubertool/__init__.py
+++ ubertool/__init__.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 # Add the root of the "ubertool" (git) submodule to Python PATH
 # This allows the REST_UBER modules to import models from ubertool module
 # E.g.: from terrplant import terrplant_exe as terrplant
